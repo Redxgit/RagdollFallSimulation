e0ba2622759619ba547f59b84c97199b5dc00172
Redxgit <17arman08@gmail.com>
2018-09-26 18:08:09 +0200

    ya no toco mas

--- Assets/GatherAccelerometerData.cs
+++ Assets/GatherAccelerometerData.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor.Experimental.UIElements;
 using UnityEngine;
 
@@ -11,6 +12,12 @@ public class GatherAccelerometerData : MonoBehaviour {
 	[SerializeField] private List<Vector3> gravityDir;
 	[SerializeField] private List<float> deltaT;
 	[SerializeField] private Transform objToTrack;
+
+	[SerializeField] private int[] windowSizes;
+
+	[SerializeField] private char commentaryChar = '%';
+
+	public bool isFall;
 
 	public float lastTime;
 
@@ -78,7 +85,8 @@ public class GatherAccelerometerData : MonoBehaviour {
 			Mathf.DeltaAngle(objToTrack.eulerAngles.z, lastAngle.z)));
 		lastAngle = objToTrack.eulerAngles;
 		lastPos = objToTrack.position;
-		gravityDir.Add(Vector3.Project(Physics.gravity, -objToTrack.up));
+		//Vector3 gravityProj = Vector3.Project(Physics.gravity, -objToTrack.up);
+		gravityDir.Add( objToTrack.TransformDirection(Physics.gravity));
 	}
 
 	// Update is called once per frame
@@ -93,5 +101,69 @@ public class GatherAccelerometerData : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.P)) {
 			Debug.Log("Valid fall, processing and saving ...");
 		}
+	}
+
+	public void ProcessData() {
+		List<Vector3> accelerations = new List<Vector3>();
+		
+		//Aqui seria calcular las aceleraciones desde los datos recogidos
+		
+		for (int i = 0; i < deltaPosition.Count; i++) {
+			accelerations.Add(deltaPosition[i]);
+		}
+
+		//List<Vector3[]> windowsData = new List<Vector3[]>();
+		List<windowData> windows = new List<windowData>();
+		List<string> windowInfo = new List<string>();
+		for (int i = 0; i < windowSizes.Length; i++) {
+			if (accelerations.Count > windowSizes[i]) {
+				var windowData = windows[i];
+				float[] accelsX = new float[windowSizes[i]];
+				float[] accelsY = new float[windowSizes[i]];
+				float[] accelsZ = new float[windowSizes[i]];
+				windowData.accelerationX = new float[accelerations.Count - windowSizes[i]];
+				for (int j = 0; j < accelerations.Count - windowSizes[i]; j++) {
+					for (int k = 0; k < windowSizes[i]; k++) {
+						windowData.accelerationX[k] = accelerations[j + k].x;
+						accelsX[k] = accelerations[j + k].x;
+						windowData.accelerationY[k] = accelerations[j + k].y;
+						accelsY[k] = accelerations[j + k].y;
+						windowData.accelerationZ[k] = accelerations[j + k].z;
+						accelsZ[k] = accelerations[j + k].z;
+					}
+
+					windowData.meanX = MathUtils.MyMean(accelsX);
+					windowData.meanY = MathUtils.MyMean(accelsY);
+					windowData.meanZ = MathUtils.MyMean(accelsZ);
+
+					windowData.stdDevX = MathUtils.MyStdDev(accelsX, windowData.meanX);
+					windowData.stdDevY = MathUtils.MyStdDev(accelsY, windowData.meanY);
+					windowData.stdDevZ = MathUtils.MyStdDev(accelsZ, windowData.meanZ);
+
+					windowData.isFall = isFall;
+				}
+			}
+			else {
+				Debug.LogError("WindowSizeTooBig");
+				return;
+			}
+		}
+
+		string fullData = commentaryChar + "fall accelerations + \n\n";
+	}
+
+	[System.Serializable]
+	public struct windowData {
+		public int numWindow;
+		public float[] accelerationX;
+		public float[] accelerationY;
+		public float[] accelerationZ;
+		public float meanX;
+		public float meanY;
+		public float meanZ;
+		public float stdDevX;
+		public float stdDevY;
+		public float stdDevZ;
+		public bool isFall;
 	}
 }
